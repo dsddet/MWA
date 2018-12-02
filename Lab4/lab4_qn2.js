e7c4300ef7cd319327592533219a9dbbfb7b6dbf
Deusdedit <dedit.dlutwama@gmail.com>
2018-12-02 07:18:19 -0600

    submission

--- Lab4/lab4_qn2.js
+++ Lab4/lab4_qn2.js
@@ -1,4 +1,33 @@
 const os = require('os');
+const { BehaviorSubject } = require('rxjs');
 
-console.log(os.cpus());
-console.log(Math.round(os.totalmem() / 1024000000));
+// console.log(os.cpus().length);
+// console.log(Math.round(os.totalmem() / 1024000000));
+
+
+function checkSystem() {
+    const bSubject$ = new BehaviorSubject("Checking your system");
+    bSubject$.subscribe((x) => { console.log(x) });
+    let pass = true;
+
+
+    if (!(os.cpus().length >= 2)) {
+        bSubject$.next("Processor not supported");
+        pass = false;
+    }
+
+    if (!(Math.round(os.totalmem() / 1024000000) >= 4)) {
+        bSubject$.next("This app needs at least 4 GB of RAM");
+        pass = false;
+    }
+
+    if (pass) {
+        bSubject$.next("System is checked successfully")
+    }
+
+
+
+
+}
+
+checkSystem();
